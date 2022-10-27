1b1c27733415d30d09e7ed4727ca78864a7793a6
Hindjanati <100366241+Hindjanati@users.noreply.github.com>
2022-10-27 18:02:11 +0100

    added Reactive Form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, NgModel } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ShareServiceService } from '../Services/share-service.service';
 
@@ -10,19 +10,42 @@ import { ShareServiceService } from '../Services/share-service.service';
 })
 export class HomeComponent implements OnInit {
 
-  myForm = new FormGroup({
-    fName: new FormControl(),
-    lName: new FormControl(),
-    age: new FormControl()
-  })
+  // myForm = new FormGroup({
+  //   fName: new FormControl(),
+  //   lName: new FormControl(),
+  //   age: new FormControl()
+  // })
+  myForm: any;
   constructor( private route: Router,
-    private service: ShareServiceService
+    private service: ShareServiceService, private formbuilder: FormBuilder
     ) { 
       // this.service.print();
       console.log(this.service.data)
+
+      this.myForm = formbuilder.group({
+        fName: ['', [Validators.required, Validators.minLength(3)]],
+        lName: ['', Validators.required],
+        age: [Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        items: this.formbuilder.array([
+          this.formbuilder.group({
+            id:['1'],
+            title: ['title1']
+          }),
+          this.formbuilder.group({
+            id:['2'],
+            title: ['title2']
+          })
+        ]),
+      inputs: this.formbuilder.array([
+        this.formbuilder.group({
+          id:['1'],
+        })
+      ])
+    })
     }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   Nom: string|undefined;
@@ -40,7 +63,48 @@ export class HomeComponent implements OnInit {
   }
 
   print(){
-    console.log(this.myForm);
+    console.log(this.myForm.value);
+  }
+
+  get fname(){
+    return this.myForm.fName;
+  }
+
+  get lname(){
+    return this.myForm.lName;
+  }
+
+  get age(){
+    return this.myForm.age;
+  }
+
+  get email(){
+    return this.myForm.email;
+  }
+
+  get items(){
+    return this.myForm.get('items') as FormArray
+  }
+
+  get inputs(){
+    return this.myForm.get('inputs') as FormArray
+  }
+
+  addNewItem(){
+    let newInput = this.formbuilder.group({
+      id:['']
+    })
+    this.items.push(newInput);
   }
  
+  Delete(i: any){
+    this.items.removeAt(i);
+  }
+  
+  addNewInput(){
+    let newInput = this.formbuilder.group({
+      id:['']
+    })
+    this.inputs.push(newInput);
+  }
 }
